7be8a027be24bb996ce5b957a1a0d581cf607963
Benneth-Bejamas <benneth@bejamas.io>
2021-12-13 15:44:25 +0100

    gatsby storyblok configuration

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,14 @@ module.exports = {
       resolve: "gatsby-plugin-root-import",
       options: { "@": path.join(__dirname, "src") },
     },
+    {
+      resolve: "gatsby-source-storyblok",
+      options: {
+        accessToken: "aTUpUwzzQqbtUJ0pRpBB2Att",
+        version: "draft",
+        localAssets: true, // Optional parameter to download the images to use with Gatsby Image Plugin
+        //languages: ["de", "at"], // Optional parameter. Omission will retrieve all languages by default.
+      },
+    },
   ],
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,38 @@
 import React from "react";
+import { graphql } from "gatsby";
 
-const HomePage = () => <div>HomePage got it</div>;
+interface IProps {
+  content: string;
+  name: string;
+  title: string;
+}
+interface IHome {
+  storyblokEntry: IProps;
+}
+
+interface Data extends IHome {
+  data: IHome;
+}
+const HomePage = ({ data }: Data) => {
+  const story = data.storyblokEntry;
+  console.log(data);
+
+  const content = JSON.parse(story.content);
+  console.log("content", content);
+  return (
+    <div>
+      HomePage got it {story.name} {content.title}
+    </div>
+  );
+};
 
 export default HomePage;
+
+export const query = graphql`
+  query HomeQuery {
+    storyblokEntry(full_slug: { eq: "home" }) {
+      content
+      name
+    }
+  }
+`;
